29980349c4ea92f4e17f3c37d2b88c9eae1da393
NARENDARAN M <narenrdkn@gmail.com>
2026-07-04 14:29:27 +0530

    Time: 9 ms (10.74%), Space: 44.7 MB (9.23%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+class Solution {
+    public String multiply(String num1, String num2) {
+       BigInteger num11= new BigInteger(num1);
+       BigInteger num22=new BigInteger(num2);
+       BigInteger product=num11.multiply (num22);
+   return product.toString();
+        
+    }
+}
