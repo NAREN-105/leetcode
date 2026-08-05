6a1a74097e0ca9bb3622f37802560b4fe7c881a9
NARENDARAN M <narenrdkn@gmail.com>
2026-08-05 21:23:35 +0530

    Time: 1 ms (77.47%), Space: 44.2 MB (39.67%) - LeetHub

---
+++ 3996-even-number-of-knight-moves/3996-even-number-of-knight-moves.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean canReach(int[] start, int[] target) {
+        int st=(start[0]+start[1])%2;
+        int tar=(target[0]+target[1])%2;
+        return st==tar;
+    }
+    
+}
