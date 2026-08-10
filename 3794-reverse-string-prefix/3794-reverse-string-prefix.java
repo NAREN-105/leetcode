0fe3e5cdb1984eec0538d5425c3e632afe96ab51
NARENDARAN M <narenrdkn@gmail.com>
2026-08-10 13:58:43 +0530

    Time: 2 ms (45.22%), Space: 44.8 MB (34.15%) - LeetHub

--- 3794-reverse-string-prefix/3794-reverse-string-prefix.java
+++ 3794-reverse-string-prefix/3794-reverse-string-prefix.java
@@ -1,19 +1,8 @@
 class Solution {
     public String reversePrefix(String s, int k) {
-    String str=""; 
-    StringBuilder sb=new StringBuilder();
-    for(int i=0;i<s.length();i++)
-    {
-        if(i<k)
-    {
-    sb.append(s.charAt(i));    
-    }
-    else
-    {
-        str+=s.charAt(i);
-    } 
-       
-    }
-    return sb.reverse().toString()+str; 
-}
+    String str1=s.substring(0,k);
+    StringBuilder sb=new StringBuilder(str1);
+    String str2=s.substring(k,s.length());
+    return sb.reverse().toString()+str2; 
+    }  
 }
