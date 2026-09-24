24e193397c69ca7a4a705132b10f795ad29cb4c2
NARENDARAN M <narenrdkn@gmail.com>
2026-09-24 16:14:33 +0530

    Time: 1 ms (99.69%), Space: 45.8 MB (11.42%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -1,18 +1,14 @@
 class Solution {
     public int smallestIndex(int[] nums) {
-    int mag=Integer.MAX_VALUE;
     for(int i=0;i<nums.length;i++){
         int val=sum(nums,i);
-        if(val==i&&i<mag){
-            mag=i;
-        }
-        else
-        {
-            continue;
+        if(val==i){
+            return i;
         }
     }
-    return mag==Integer.MAX_VALUE?-1:mag;
+    return -1;
     }
+    
     public static int sum(int arr[],int n){
         int sum=0;
         int val=arr[n];
